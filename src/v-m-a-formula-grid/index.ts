839096fb23721b51b40a749c86f37e1656de02e3
xxs3315 <xxs3315@foxmail.com>
2023-11-08 10:18:40 +0800

    chore: dev

--- src/v-m-a-formula-grid/index.ts
+++ src/v-m-a-formula-grid/index.ts
@@ -2,7 +2,7 @@ import { VmaFormulaGridCore } from '../../types';
 import hooks from './hooks.ts';
 import VmaFormulaGridCompContextMenuHook from '../components/context-menu/hooks';
 
-export const version = '1.0.24';
+export const version = '1.1.0';
 
 const installedPlugins: any[] = [];
 
