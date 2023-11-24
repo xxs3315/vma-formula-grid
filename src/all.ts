907e24f2c2d69b50152c4c921978ff192914bb9c
xxs3315 <xxs3315@foxmail.com>
2023-11-24 08:58:02 +0800

    chore: 清理无用注释

--- src/all.ts
+++ src/all.ts
@@ -5,28 +5,10 @@ import dc, { dfo, d } from './utils';
 import { FormulaParser, MAX_ROW, MAX_COLUMN, SSF, DepParser, FormulaError, FormulaHelpers, Types, ReversedTypes, Factorials, WildCard, Criteria, Address } from './formula';
 
 const components = [VmaFormulaGrid];
-//
-// declare module './v-m-a-formula-grid' {
-//     interface VmaFormulaGridCore {
-//         install: typeof install
-//     }
-// }
 
 export function install(app: App) {
     components.forEach((component) => component.install(app));
 }
-
-/*// To auto-install when vue is found
-// 如果浏览器环境且拥有全局Vue，则自动安装组件
-declare global {
-    interface Window {
-        Vue?: any;
-    }
-}
-
-if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(VmaFormulaGrid);
-}*/
 
 export * from './v-m-a-formula-grid';
 
